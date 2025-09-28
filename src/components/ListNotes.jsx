1cdcbb5562fbcc6b9a81512b307ec2253d7a607d
Onurcan Yelestepe <oyelestepe1@gmail.com>
2025-09-28 18:18:44 +0300

    style updates

--- src/components/ListNotes.jsx
+++ src/components/ListNotes.jsx
@@ -2,14 +2,13 @@ import React from 'react'
 
 function ListNotes({notes, deleteNote}) {
   return (
-    <div>
-        {
-            notes.map((note) => (
-                <div key={note.id}>
-                    <h2>{note.title}</h2>
-                    <p>{note.content}</p>
-                    <span>{note.category}</span>
-                    <button onClick={() => deleteNote(note.id)}>X</button>
+    <div className='notes-container'>
+        { notes.map((note) => (
+                <div className='note' key={note.id}>
+                    <h2 className='font-bold text-lg mb-2'>{note.title}</h2>
+                    <p className='text-sm mb-2'>{note.content}</p>
+                    <span className='px-2 py-1 rounded bg-yellow-400 text-white font-semibold text-xs'>{note.category}</span>
+                    <button className='absloute top-1 right-2 text-red-500 hover:text-red-700' onClick={() => deleteNote(note.id)}>X</button>
                 </div>
             ))
         }
--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -15,18 +15,22 @@ function handleTakeNote(e){
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-        <label>Title :</label>
-        <input type='text' name='title' value={formData.title} onChange={handleTakeNote}/>
-        <label>Content :</label>
-        <textarea type='text' name='content' value={formData.content} onChange={handleTakeNote}/>
-        <label>Choose a category :</label>
-        <select name='category' value={formData.category} onChange={handleTakeNote}>
+    <form className='flex flex-col w-lg border border-gray-300 border-solid p-3 m-2 rounded-xl '
+      onSubmit={handleSubmit}>
+        <label className='form-label'>Title :</label>
+        <input className='form-input' 
+          placeholder='Enter title' type='text' name='title' value={formData.title} onChange={handleTakeNote}/>
+        <label className='form-label'>Content :</label>
+        <textarea className='form-input' 
+          placeholder='Enter your content...' type='text' name='content' value={formData.content} onChange={handleTakeNote}/>
+        <label className='form-label'>Choose a category :</label>
+        <select className='form-input'
+          name='category' value={formData.category} onChange={handleTakeNote}>
             <option value="Personal">Personal</option>
             <option value="Work">Work</option>
             <option value="Study">Study</option>
         </select>
-        <button type='submit'>Take Note</button>
+        <button className='btn btn-primary' type='submit'>Take Note</button>
     </form>
   )
 }
