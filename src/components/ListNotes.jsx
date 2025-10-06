6100e778843a5f1792d66db13392a9e5391b6119
Onurcan Yelestepe <oyelestepe1@gmail.com>
2025-10-06 17:49:18 +0300

    refactor ListNotes component for improved styling and structure

--- src/components/ListNotes.jsx
+++ src/components/ListNotes.jsx
@@ -4,10 +4,27 @@ import { MdDeleteForever } from "react-icons/md";
 
 
 function ListNotes({notes, deleteNote, startEdit, handleDeleteClick}) {
+  const categoryStyles  = {
+    personal: "bg-blue-200 border-blue-400",
+    study: "bg-green-200 border-green-400",
+    work: "bg-yellow-200 border-yellow-400"
+  }
+
+  const labelStyles = {
+    personal: "bg-blue-500",
+    study: "bg-green-500",
+    work: "bg-yellow-500",
+  };
+
+  
   return (
     <div className='notes-container'>
-        { notes.map((note) => (
-                <div className='note' key={note.id}>
+        { notes.map((note) => {
+            const key = (note.category || "other").toString().trim().toLowerCase();
+            const cardClass = categoryStyles[key] || "bg-gray-50 border-gray-300";
+            const tagClass = labelStyles[key] || "bg-gray-400";
+            return(
+                <div  key={note.id} className={`note border ${cardClass}`}>
                 <div className='flex justify-between'>
                   <h2 className='font-bold font-indie text-lg mb-2'>{note.title}</h2>
                   <div className='flex mt-0 p-0'>
@@ -15,10 +32,10 @@ function ListNotes({notes, deleteNote, startEdit, handleDeleteClick}) {
                     <button className='btn m-1 p-1 text-xl text-blue-500 hover:text-blue-700' onClick={() => startEdit(note)}><FaEdit /></button>
                   </div>
                 </div>
-                    <span className='px-2 py-1 rounded bg-yellow-400 text-white font-semibold text-xs'>{note.category}</span>
+                    <span className={`px-2 py-1 rounded ${tagClass} text-white font-semibold text-xs`}>{note.category}</span>
                     <p className='text-sm mb-2 mt-2 font-indie font-normal'>{note.content}</p>
                 </div>
-            ))
+            )})
         }
     </div>
   )
